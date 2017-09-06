12e9a8f71a8150391612b1b45f314e83fe3e3327
Jono <software@avonwood.co.uk>
2017-09-06 17:00:03 +0100

    Initial implementation of diagnostic analyzer

--- ControllerHttpAttributeAnalyzer/ControllerHttpAttributeAnalyzer/DiagnosticAnalyzer.cs
+++ ControllerHttpAttributeAnalyzer/ControllerHttpAttributeAnalyzer/DiagnosticAnalyzer.cs
@@ -20,7 +20,7 @@ namespace ControllerHttpAttributeAnalyzer
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Attributes";
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
@@ -28,22 +28,28 @@ namespace ControllerHttpAttributeAnalyzer
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSymbolAction(AnalyzeMethod, SymbolKind.Method);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        /// <summary>
+        /// If the method is within a class that inherits from Controller, 
+        /// then check it has an attribute that inherits from HttpMethodAttribute
+        /// </summary>
+        /// <param name="context"></param>
+        private static void AnalyzeMethod(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            var methodSymbol = (IMethodSymbol)context.Symbol;
+            if (methodSymbol.ContainingType.BaseType.Name.EndsWith("Controller"))
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                foreach (var attribute in methodSymbol.GetAttributes())
+                {
+                    if (attribute.AttributeClass.BaseType.Name.Equals("HttpMethodAttribute"))
+                    {
+                        return;
+                    }
+                }
 
+                var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], methodSymbol.Name);
                 context.ReportDiagnostic(diagnostic);
             }
         }
